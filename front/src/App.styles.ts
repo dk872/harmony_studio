c7642998df401f9b842e15ddd6291ee2333595f8
dk872 <dimakulyk2005@gmail.com>
2025-10-26 16:31:18 +0200

    Add main app container and layout styles

---
+++ front/src/App.styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+`;
+
+export const MainContent = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
