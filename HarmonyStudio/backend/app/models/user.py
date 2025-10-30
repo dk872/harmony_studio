72b59b47fac38ab96134a64b48cae8d50a176ef7
Nastya Glushchenko <gluscheko.a.s@gmail.com>
2025-10-30 20:23:22 +0200

    Add base User model

---
+++ HarmonyStudio/backend/app/models/user.py
@@ -0,0 +1,30 @@
+from backend.app.extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(10), nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    date_of_birth = db.Column(db.Date, nullable=True)
+    phone_number = db.Column(db.String(15), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "role": self.role,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "date_of_birth": self.date_of_birth,
+            "phone_number": self.phone_number,
+            "email": self.email
+        }
