445da9b4aba74a9dbf45f1263374e9d26d0a43b4
dk872 <dimakulyk2005@gmail.com>
2025-11-11 17:35:00 +0200

    Add jest.config.cjs file

---
+++ front/jest.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!axios)/",
+  ],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/__mocks__/fileMock.js",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
